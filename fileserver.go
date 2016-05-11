7e70debb2eae325a7cd5910a5065b1d9481794f8
Landon Wainwright <landon.wainwright@bet365.com>
2016-05-11 12:01:19 +0100

    Change message

--- fileserver.go
+++ fileserver.go
@@ -14,10 +14,10 @@ func main() {
 	flag.StringVar(&addr, "addr", ":8090", "The local address to bind")
 	flag.Parse()
 
-	// Add the file server for the asset directory
+	// Add the file server ontop of the provided file directory
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(filedir))))
 
 	// Start the server
-	fmt.Printf("Starting server using address: %s", addr)
+	fmt.Printf("Starting fileserver using bind address: %s", addr)
 	http.ListenAndServe(addr, nil)
 }
